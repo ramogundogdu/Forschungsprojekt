19cb55cb9e43e18e21be17fbc7ce0197b794c5a5
Felix Gruner <felix.gruner@gmail.com>
2015-05-26 17:58:35 +0200

    able to load, show and process ply-data in matlab

--- Sequence/SceneFlow/sceneFlowTest.m
+++ Sequence/SceneFlow/sceneFlowTest.m
@@ -9,3 +9,13 @@ IRightT1 = im2double(imread('SceneFlowTestData/sfTest_066_right.png'));
 
 load(['Versuch3_final/Callib_Versuch3_Cut_Complete_L2R.mat']);
 load(['SceneFlowTestData/SceneFlowTestData.mat']);
+
+Ply_file = 'SceneFlowTestData/Pointcloud_sfTestData_mesh_ascii.ply'
+
+
+% convert ply to processable tri-mesh data
+[ Mesh_ConnectivityList, Mesh_Vertex_xyz ] = ply_read ( Ply_file, 'tri' );
+
+% trisurf testmesh
+trisurf ( Mesh_ConnectivityList', Mesh_Vertex_xyz(1,:), Mesh_Vertex_xyz(2,:), Mesh_Vertex_xyz(3,:) );
+axis equal;
